8d77fbf2f2bedf810224349985258a2e1848fad2
Vinicius Santana <santanaviny@gmail.com>
2021-07-28 11:21:40 -0700

    Pseudo code start

---
+++ todo.js
@@ -0,0 +1,41 @@
+/** 
+ * Add TODO 
+ **/  
+
+// add todo when user click enter on input or click outisde input
+  // create to do
+  // append new todo to todos list
+  // update count
+
+/** 
+ * Edit TODO 
+ **/  
+
+// Clicking on todo checkbox should make that todo completed
+// Clicking on todo text twice should change text to input
+  // Clicking on enter or outside input should save change to todo
+
+/** 
+ * Delete TODO 
+ **/  
+
+// Clicking on X should remove/delete todo from list
+// update count
+
+
+/** 
+ * Filter TODOs
+ **/
+
+// Todo list should show only todos that match the filter selection
+
+
+/** 
+ * Toggle all TODOs - Completed x Active
+ **/
+
+
+
+/** 
+ * Clear all Completed
+ **/
